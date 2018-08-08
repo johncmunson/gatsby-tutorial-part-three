26f8d79231849e5586750895126bd45f3dc5fe31
John Munson <johncmunson91@gmail.com>
2018-08-08 01:04:55 -0500

    updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,27 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from "react"
+import { Link } from "gatsby"
 
 const ListLink = ({ to, children }) => (
-  <li style={{ display: "inline-block", marginRight: `1rem` }}>
-    <Link to={to}>{children}</Link>
-  </li>
-);
+    <li style={{ display: "inline-block", marginRight: `1rem` }}>
+        <Link to={to}>{children}</Link>
+    </li>
+)
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
-    <header style={{ marginBottom: `1.5rem` }}>
-      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>MySweetSite</h3>
-      </Link>
-      <ul style={{ listStyle: `none`, float: "right" }}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>
-    {children}
-  </div>
-);
+    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+        <header style={{ marginBottom: `1.5rem` }}>
+            <Link
+                to="/"
+                style={{ textShadow: `none`, backgroundImage: `none` }}
+            >
+                <h3 style={{ display: `inline` }}>MySweetSite</h3>
+            </Link>
+            <ul style={{ listStyle: `none`, float: "right" }}>
+                <ListLink to="/">Home</ListLink>
+                <ListLink to="/about/">About</ListLink>
+                <ListLink to="/contact/">Contact</ListLink>
+            </ul>
+        </header>
+        {children}
+    </div>
+)
